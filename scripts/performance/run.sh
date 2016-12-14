b2cca7af15a617efeba4a52707310866a1499bfe
Supun Kamburugamuve <supun06@gmail.com>
2016-12-14 18:20:07 -0500

    addn

--- scripts/performance/run.sh
+++ scripts/performance/run.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 #sizes=( 1000 2000 4000 1000 2000 4000 8000 16000 32000 64000 128000 256000 512000)
-sizes=( 32000 32000 32000 16000 64000 128000 256000 512000 1024000 2048000)
+sizes=( 32000 32000 32000 16000 64000 128000 256000 512000 1024000 )
 #sizes=( 512000)
 no_msgs=4000
 amqp_url="amqp://d001:5672"
 delay=25
-results=test2/latency/tcp5
+results=tcp_original/latency2
 
 for size in "${sizes[@]}"
 do
