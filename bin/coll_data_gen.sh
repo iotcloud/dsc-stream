017a77b5aa51c7b46cc2993bfbbea393e87068a0
Supun Kamburugamuve <supun06@gmail.com>
2017-05-30 23:45:06 -0400

    adding results

--- bin/coll_data_gen.sh
+++ bin/coll_data_gen.sh
@@ -2,18 +2,18 @@
 
 sizes=( 32 32 32 16 32 64 128 256 512 1024 )
 sizes=( 32 32 )
-sizes=( 32000 32000 32000 16000 64000 128000 256000 512000 1024000 )
+sizes=( 1000 1000 2000 4000 8000 16000 32000 )
 #sizes=( 32000 32000 32000 16000 64000 )
-#sizes=( 64000)
+sizes=(16000 16000 32000)
 no_msgs=4000
 amqp_url="amqp://j-080:5672"
-delay=10
+delay=400
 #results=tcp_copy/latency4
-results=march17/verbs/latency
+results=col/16_128_reduce2
 
 for size in "${sizes[@]}"
 do
   :
- java -Xms4G -Xmx4G -cp ../collectives/target/dsc-stream-collectives-0.1-jar-with-dependencies.jar edu.indiana.soic.dsc.stream.collectives.DataGenerator edu.indiana.soic.dsc.stream.perf.DataGenerator $amqp_url $results/$size $delay $size $no_msgs
+ java -Xms4G -Xmx4G -cp ../collectives/target/dsc-stream-collectives-0.1-jar-with-dependencies.jar edu.indiana.soic.dsc.stream.collectives.DataGenerator $amqp_url $results/$size $delay $size $no_msgs
  sleep 10
 done
